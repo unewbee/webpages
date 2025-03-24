5203fc44470e1201dd9c3087a0a5d4cc37efd363
channing <channing@channingdeMacBook-Air.local>
2025-03-24 12:08:43 +1100

    update

--- src/components/ui/icon-cloud.tsx
+++ src/components/ui/icon-cloud.tsx
@@ -38,13 +38,13 @@ export const cloudProps: Omit<ICloud, "children"> = {
 };
 
 export const renderCustomIcon = (icon: SimpleIcon, theme: string) => {
-  const bgHex = theme === "light" ? "#f3f2ef" : "#080510";
+  // const bgHex = theme === "light" ? "#f3f2ef" : "#080510";
   const fallbackHex = theme === "light" ? "#6e6e73" : "#ffffff";
-  const minContrastRatio = theme === "dark" ? 2 : 1.2;
+  const minContrastRatio = theme === "dark" ? 2 : 0.9;
 
   return renderSimpleIcon({
     icon,
-    bgHex,
+    // bgHex,
     fallbackHex,
     minContrastRatio,
     size: 42,
